4568dab4339847fe4b5de20a2cb8d3851a0c92c2
Akash <hafejmohammadakash@gmail.com>
2025-04-07 03:34:46 -0600

    conditonal rendaring option done

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -6,21 +6,35 @@ import React from 'react'
  function Todo({task, isDone}) {
 
 // conditional rendering option 1    
-    // if(isDone === true){
-    //    return <li>Finish: {task}</li>
-    // }else{
-    //     return <li>worl on: {task}</li>
-    // }
+    if(isDone === true){
+       return <li>Finish: {task}</li>
+    }else{
+        return <li>worl on: {task}</li>
+    }
 
 // conditional rendering option 2
-    // if(isDone ===true){
-    //     return <li>Finish: {task}</li>
-    // }
-    // return  <li>worl on: {task}</li>
+    if(isDone ===true){
+        return <li>Finish: {task}</li>
+    }
+    return  <li>worl on: {task}</li>
 
 // conditional rendering option 3
 return <li>{isDone ? "Finished" : "Work On"} : {task}</li>
 
+// conditional rendering option 4
+return <li> {task} {isDone && ": Finished"}</li>
+
+// conditional rendering option 5
+return <li> {task} {isDone || ": do it"}</li>
+
+// conditional rendering option 6
+let listItems = '';
+if(isDone === true){
+    listItems = <li>Finish: {task}</li>
+}else{
+    listItems = <li>worl on: {task}</li>
+}
+return listItems
 }
 
 export default Todo;
