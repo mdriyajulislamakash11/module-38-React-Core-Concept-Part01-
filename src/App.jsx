a31cd7f0bdbac5156250fca8731ac43135d98cad
Akash <hafejmohammadakash@gmail.com>
2025-04-06 23:54:57 -0600

    app.css theke ami kichu style custom vabeb aply korlam

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      
+       
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Product></Product>
     </>
+  )
+}
+
+function Product (){
+  return(
+    <div className='box'>
+      <h3 className='box'>Hello buyer</h3>
+      <p className='box'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ab eius a vel ipsam, natus doloribus mollitia magni. Blanditiis ab, consequatur totam natus fugit libero eligendi eveniet obcaecati modi asperiores.</p>
+    </div>
   )
 }
 
